9d5c73ccf1a2abbf80850aab67570a764f7931ba
Adam Hradil <hradiladam@gmail.com>
2024-11-30 13:54:21 +0100

    C4: improve html and css - minor changes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+/* Dark theme switch */
+
+const themeToggle = document.querySelector('.theme-toggle');
+  themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+  });
