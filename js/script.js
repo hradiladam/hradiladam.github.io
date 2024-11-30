e542410c26d5c7a28456d90f61d237b5ba326e83
Adam Hradil <hradiladam@gmail.com>
2024-11-30 21:13:00 +0100

    C5: add 'go to the top' functionality, change css dark mode button, change visuals

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-
 /* Dark theme switch */
-
 const themeToggle = document.querySelector('.theme-toggle');
-  themeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-  });
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+
+  if (document.body.classList.contains('dark-mode')) {  // Corrected class and method
+    themeToggle.innerHTML = '<i class="fa-regular fa-sun"></i>';
+  } else {
+    themeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+  }
+});
+
+
+/* Scrolls to the top with navigation-arrow element */
+const button = document.querySelector('.navigation-arrow');
+button.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
